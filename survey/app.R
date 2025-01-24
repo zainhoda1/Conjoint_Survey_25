01ec87dcc9516d2cb07434d4b318a46aa09c7d1c
Zain Hoda <zainhoda1@gmail.com>
2025-01-24 10:41:51 -0500

    survey updated

--- survey/app.R
+++ survey/app.R
@@ -18,6 +18,43 @@ db <- sd_database(
 
 # Server setup
 server <- function(input, output, session) {
+  
+  # Define vector of car image options
+  
+  car_images <- c("car_small_5", "car_mid_5", "car_large_5",
+                  "suv_small_5", "suv_mid_5", "suv_large_5")
+  src <- paste0(
+    '<img src="images/car-images/', car_images, '.jpg" width=100%>'
+  )
+  names(car_images) <- src
+  
+  # Define first choice question
+  sd_question(
+    type  = 'mc_buttons',
+    id    = 'images',
+    label = "Choose one",
+    option = car_images
+  )
+  
+  # Run observer that updates the chosen_image when an image is chosen
+  observe({
+    
+    # This updates whenever input$images changes
+    chosen_src <- paste0(
+      'images/car-images/', input$images, '.jpg'
+    )
+    
+    # Update the rendered image
+    output$chosen_image <- renderImage({
+      list(
+        src = chosen_src,
+        contentType = "image/jpg",
+        width = "100%"
+      )
+    }, deleteFile = FALSE)
+    
+  })
+  
 
   survey <- read_csv(here('data', 'survey.csv'))
   respondentID <- sample(survey$respID, 1)
@@ -29,17 +66,35 @@ server <- function(input, output, session) {
   q1_alt2 <- q1_alts %>% filter(altID == 2)
   q1_alt3 <- q1_alts %>% filter(altID == 3)
   
-  q1_alts2 <- df %>% filter(qID == 2)
-  q1_alt1_2 <- q1_alts2 %>% filter(altID == 1)
-  q1_alt2_2 <- q1_alts2 %>% filter(altID == 2)
-  q1_alt3_2 <- q1_alts2 %>% filter(altID == 3)
+  q2_alts <- df %>% filter(qID == 2)
+  q2_alt1 <- q2_alts %>% filter(altID == 1)
+  q2_alt2 <- q2_alts %>% filter(altID == 2)
+  q2_alt3 <- q2_alts %>% filter(altID == 3)
+  
+  q3_alts <- df %>% filter(qID == 3)
+  q3_alt1 <- q3_alts %>% filter(altID == 1)
+  q3_alt2 <- q3_alts %>% filter(altID == 2)
+  q3_alt3 <- q3_alts %>% filter(altID == 3)
+  
+  q4_alts <- df %>% filter(qID == 4)
+  q4_alt1 <- q4_alts %>% filter(altID == 1)
+  q4_alt2 <- q4_alts %>% filter(altID == 2)
+  q4_alt3 <- q4_alts %>% filter(altID == 3)
+  
+  q5_alts <- df %>% filter(qID == 5)
+  q5_alt1 <- q5_alts %>% filter(altID == 1)
+  q5_alt2 <- q5_alts %>% filter(altID == 2)
+  q5_alt3 <- q5_alts %>% filter(altID == 3)
+  
+  q6_alts <- df %>% filter(qID == 6)
+  q6_alt1 <- q6_alts %>% filter(altID == 1)
+  q6_alt2 <- q6_alts %>% filter(altID == 2)
+  q6_alt3 <- q6_alts %>% filter(altID == 3)
 
   cost_com <- q1_alt2$price
   sd_store_value(cost_com)
   
   
-  cost_array <- q1_alts$price
-  sd_store_value(cost_array)
   
 
   observeEvent(input$budget, {
@@ -56,14 +111,61 @@ server <- function(input, output, session) {
     q1c3 <- as.numeric(input$budget) * q1_alt3$price
     sd_store_value(q1c3)
     
-    q2c1 <- as.numeric(input$budget) * q1_alt1_2$price
+    q2c1 <- as.numeric(input$budget) * q2_alt1$price
     sd_store_value(q2c1)
-    q2c2 <- as.numeric(input$budget) * q1_alt2_2$price
+    q2c2 <- as.numeric(input$budget) * q2_alt2$price
     sd_store_value(q2c2)
-    q2c3 <- as.numeric(input$budget) * q1_alt3_2$price
+    q2c3 <- as.numeric(input$budget) * q2_alt3$price
     sd_store_value(q2c3)
 
   })
+  
+  observe(
+    {
+      q3c1 <- as.numeric(input$budget) * q3_alt1$price
+      sd_store_value(q3c1)
+      q3c2 <- as.numeric(input$budget) * q3_alt2$price
+      sd_store_value(q3c2)
+      q3c3 <- as.numeric(input$budget) * q3_alt3$price
+      sd_store_value(q3c3)
+    }
+  )
+  
+  observe(
+    {
+      q4c1 <- as.numeric(input$budget) * q4_alt1$price
+      sd_store_value(q4c1)
+      q4c2 <- as.numeric(input$budget) * q4_alt2$price
+      sd_store_value(q4c2)
+      q4c3 <- as.numeric(input$budget) * q4_alt3$price
+      sd_store_value(q4c3)
+
+    }
+  )
+  
+  observe(
+    {
+      q5c1 <- as.numeric(input$budget) * q5_alt1$price
+      sd_store_value(q5c1)
+      q5c2 <- as.numeric(input$budget) * q5_alt2$price
+      sd_store_value(q5c2)
+      q5c3 <- as.numeric(input$budget) * q5_alt3$price
+      sd_store_value(q5c3)
+      
+    }
+  )
+  
+  observe(
+    {
+      q6c1 <- as.numeric(input$budget) * q6_alt1$price
+      sd_store_value(q6c1)
+      q6c2 <- as.numeric(input$budget) * q6_alt2$price
+      sd_store_value(q6c2)
+      q6c3 <- as.numeric(input$budget) * q6_alt3$price
+      sd_store_value(q6c3)
+      
+    }
+  )
   
 
 
