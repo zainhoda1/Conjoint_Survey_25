787b79710fc88c7265c68733388b21ac83365147
Zain Hoda <zainhoda1@gmail.com>
2025-01-28 16:20:11 -0500

    survey updated

--- code files/make_choice_questions.R
+++ code files/make_choice_questions.R
@@ -8,12 +8,15 @@
 # Load libraries
 library(cbcTools)
 library(tidyverse)
+library(here)
 
 # Define profiles with attributes and levels
 profiles <- cbc_profiles(
-  type      = c('Fuji', 'Gala', 'Honeycrisp', 'Pink Lady', 'Red Delicious'),
-  price     = seq(1, 4, 0.5), # $ per pound
-  freshness = c('Excellent', 'Average', 'Poor')
+  powertrain      = c('Gasoline', 'Electric', 'Plug-in Hybrid'),
+  price     = seq(0.8,1.20,0.1),
+  range = seq(50, 250, 25),
+  mileage = seq(20, 60, 5),
+  operating_cost = seq(6, 21, 3)
 )
 
 # Make a basic survey using the full factorial of all profiles
@@ -28,14 +31,33 @@ head(design) # preview
 
 # Add image names matched to the apple type
 # (we'll use these to display images in the survey)
-image_names <- data.frame(
-  type = c('Fuji', 'Gala', 'Honeycrisp', 'Pink Lady', 'Red Delicious'),
-  image = c('fuji.jpg', 'gala.jpg', 'honeycrisp.jpg', 'pinkLady.jpg',
-            'redDelicious.jpg')
-)
-design <- design %>%
-  left_join(image_names, by = "type")
-head(design) # preview
+# image_names <- data.frame(
+#   type = c('Fuji', 'Gala', 'Honeycrisp', 'Pink Lady', 'Red Delicious'),
+#   image = c('fuji.jpg', 'gala.jpg', 'honeycrisp.jpg', 'pinkLady.jpg',
+#             'redDelicious.jpg')
+# )
+# design <- design %>%
+#   left_join(image_names, by = "type")
+# head(design) # preview
+
+
+design$range[design$powertrain =='Gasoline']= 0
+
+duplicates <- design[duplicated(design[c('respID', 'qID', 'powertrain', 'price',
+                                       'range', 'mileage', 'operating_cost' )]), ]
+
+design <- design[!duplicated(design[c('respID', 'qID', 'powertrain', 'price',
+                            'range', 'mileage', 'operating_cost' )]), ]
+
+#test <- design %>% filter(respID == 342)
+
+id_to_remove <- duplicates$respID
+
+design <- design %>% 
+  filter(!respID %in% id_to_remove)
+
 
 # Save design
-write_csv(design, 'choice_questions.csv')
+write_csv(design, here('data', 'choice_questions.csv'))
+
+
--- survey/app.R
+++ survey/app.R
@@ -3,8 +3,11 @@ library(surveydown)
 library(tidyverse)
 library(here)
 library(glue)
+library(dplyr)
 
 # Database setup
+
+#db <- sd_db_connect()
 
 db <- sd_database(
   host   = "",
@@ -18,32 +21,61 @@ db <- sd_database(
 
 # Server setup
 server <- function(input, output, session) {
-  
+
+
+
+
+
   # Define vector of car image options
-  
-  car_images <- c("car_small_5", "car_mid_5", "car_large_5",
+
+  sedan_images <- c("car_small_5", "car_mid_5", "car_large_5",
+                  "car_small_4", "car_mid_4", "car_large_4")
+
+  suv_images <- c("suv_small_4", "suv_mid_4", "suv_large_4",
                   "suv_small_5", "suv_mid_5", "suv_large_5")
-  src <- paste0(
-    '<img src="images/car-images/', car_images, '.jpg" width=100%>'
+
+  car_images <- c("car_small_5", "car_mid_5", "car_large_5",
+                  "car_small_4", "car_mid_4", "car_large_4",
+                  "suv_small_4", "suv_mid_4", "suv_large_4",
+                  "suv_small_5", "suv_mid_5", "suv_large_5")
+
+
+  #Choosing Car type
+
+   chosen <- car_images
+   #chosen1 <- input$next_vehicle_type
+
+  # observeEvent({
+  # chosen <- case_when(
+  #   as.integer(input$next_vehicle_type) == 1 ~ 'sedan_images',
+  #   as.integer(input$next_vehicle_type) == 2 ~ 'suv_images',
+  #   .default = 'car_images'
+  # )
+  # })
+
+  src <- glue::glue(
+    '<img src="images/car-images/{chosen}.jpg" width=100%>'
   )
-  names(car_images) <- src
-  
+  names(chosen) <- src
+
   # Define first choice question
   sd_question(
     type  = 'mc_buttons',
     id    = 'images',
-    label = "Choose one",
-    option = car_images
+    label = "Which type of car are you planning to buy?",
+    option = chosen
   )
-  
+
+
+
   # Run observer that updates the chosen_image when an image is chosen
   observe({
-    
+
     # This updates whenever input$images changes
     chosen_src <- paste0(
       'images/car-images/', input$images, '.jpg'
     )
-    
+
     # Update the rendered image
     output$chosen_image <- renderImage({
       list(
@@ -52,11 +84,11 @@ server <- function(input, output, session) {
         width = "100%"
       )
     }, deleteFile = FALSE)
-    
-  })
-  
 
-  survey <- read_csv(here('data', 'survey.csv'))
+  })
+
+
+  survey <- read_csv(here('data', 'choice_questions.csv'))
   respondentID <- sample(survey$respID, 1)
   df <- survey %>%
     filter(respID == respondentID)
@@ -65,27 +97,27 @@ server <- function(input, output, session) {
   q1_alt1 <- q1_alts %>% filter(altID == 1)
   q1_alt2 <- q1_alts %>% filter(altID == 2)
   q1_alt3 <- q1_alts %>% filter(altID == 3)
-  
+
   q2_alts <- df %>% filter(qID == 2)
   q2_alt1 <- q2_alts %>% filter(altID == 1)
   q2_alt2 <- q2_alts %>% filter(altID == 2)
   q2_alt3 <- q2_alts %>% filter(altID == 3)
-  
+
   q3_alts <- df %>% filter(qID == 3)
   q3_alt1 <- q3_alts %>% filter(altID == 1)
   q3_alt2 <- q3_alts %>% filter(altID == 2)
   q3_alt3 <- q3_alts %>% filter(altID == 3)
-  
+
   q4_alts <- df %>% filter(qID == 4)
   q4_alt1 <- q4_alts %>% filter(altID == 1)
   q4_alt2 <- q4_alts %>% filter(altID == 2)
   q4_alt3 <- q4_alts %>% filter(altID == 3)
-  
+
   q5_alts <- df %>% filter(qID == 5)
   q5_alt1 <- q5_alts %>% filter(altID == 1)
   q5_alt2 <- q5_alts %>% filter(altID == 2)
   q5_alt3 <- q5_alts %>% filter(altID == 3)
-  
+
   q6_alts <- df %>% filter(qID == 6)
   q6_alt1 <- q6_alts %>% filter(altID == 1)
   q6_alt2 <- q6_alts %>% filter(altID == 2)
@@ -93,16 +125,9 @@ server <- function(input, output, session) {
 
   cost_com <- q1_alt2$price
   sd_store_value(cost_com)
-  
-  
-  
 
-  observeEvent(input$budget, {
-      mult_val <- as.numeric(input$budget) * cost_com
-      sd_store_value(mult_val)
 
-  })
-  
+
   observeEvent(input$budget, {
     q1c1 <- as.numeric(input$budget) * q1_alt1$price
     sd_store_value(q1c1)
@@ -110,7 +135,7 @@ server <- function(input, output, session) {
     sd_store_value(q1c2)
     q1c3 <- as.numeric(input$budget) * q1_alt3$price
     sd_store_value(q1c3)
-    
+
     q2c1 <- as.numeric(input$budget) * q2_alt1$price
     sd_store_value(q2c1)
     q2c2 <- as.numeric(input$budget) * q2_alt2$price
@@ -119,7 +144,7 @@ server <- function(input, output, session) {
     sd_store_value(q2c3)
 
   })
-  
+
   observe(
     {
       q3c1 <- as.numeric(input$budget) * q3_alt1$price
@@ -130,7 +155,7 @@ server <- function(input, output, session) {
       sd_store_value(q3c3)
     }
   )
-  
+
   observe(
     {
       q4c1 <- as.numeric(input$budget) * q4_alt1$price
@@ -142,7 +167,7 @@ server <- function(input, output, session) {
 
     }
   )
-  
+
   observe(
     {
       q5c1 <- as.numeric(input$budget) * q5_alt1$price
@@ -151,10 +176,10 @@ server <- function(input, output, session) {
       sd_store_value(q5c2)
       q5c3 <- as.numeric(input$budget) * q5_alt3$price
       sd_store_value(q5c3)
-      
+
     }
   )
-  
+
   observe(
     {
       q6c1 <- as.numeric(input$budget) * q6_alt1$price
@@ -163,10 +188,10 @@ server <- function(input, output, session) {
       sd_store_value(q6c2)
       q6c3 <- as.numeric(input$budget) * q6_alt3$price
       sd_store_value(q6c3)
-      
+
     }
   )
-  
+
 
 
   # Define any conditional skip logic here (skip to page if a condition is true)
