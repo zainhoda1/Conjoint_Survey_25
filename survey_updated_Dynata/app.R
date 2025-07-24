53e69050234d1c4367ae40a8061072baabf4c7b2
Pingfan Hu <pingfan0727@gmail.com>
2025-07-24 14:21:15 -0400

    removed extra codes since package is updated

--- survey_updated_Dynata/app.R
+++ survey_updated_Dynata/app.R
@@ -5,7 +5,6 @@ library(glue)
 library(dplyr)
 library(kableExtra)
 library(digest)
-library(uaparserjs)
 
 # Database setup
 db <- sd_db_connect()
@@ -160,39 +159,17 @@ server <- function(input, output, session) {
   # Make a 10-digit random number completion code
   completion_code <- sd_completion_code(10)
 
-  # Store the completion code in the survey data
-  sd_store_value(completion_code)
-
-  # Store the psid from URL
-  sd_store_value(psid(), "psid")
-
-  # Capture and store browser information using user agent
-  user_agent <- session$request$HTTP_USER_AGENT
-  if (!is.null(user_agent)) {
-    parsed_ua <- ua_parse(user_agent)
-    browser_info <- paste0(
-      parsed_ua$ua.family,
-      " v",
-      parsed_ua$ua.major,
-      ", ",
-      parsed_ua$os.family
-    )
-    sd_store_value(browser_info, "browser")
-  }
-
-  # Capture and store IP address
-  ip_address <- session$request$REMOTE_ADDR
-  sd_store_value(ip_address, "ip_address")
-
-  sd_store_value(respondentID, "respID")
-  sd_store_value(battery_respondentID, "respID")
-
   # Create random assignment when session starts
   prime_groups <- c('prime_short', 'prime_long')
   prime_group_label <- sample(c('prime_short', 'prime_long'), 1)
   names(prime_groups) <- prime_group_label
 
-  sd_store_value(prime_group_label, id = "prime_group_label")
+  # Store necessary values in the survey data
+  sd_store_value(completion_code)
+  sd_store_value(psid(), "psid")
+  sd_store_value(respondentID, "respID")
+  sd_store_value(battery_respondentID, "respID")
+  sd_store_value(prime_group_label, "prime_group_label")
 
   ## data
   df <- survey %>%
