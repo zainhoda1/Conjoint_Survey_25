b1f3cda0944b55dc4bf987d75c24d7e0e720f1e1
Zain Hoda <zainhoda1@gmail.com>
2025-10-28 08:49:39 -0400

    dynata_dropout_checks

--- quota_dashboard_Dynata/checking_survey_run1.R
+++ quota_dashboard_Dynata/checking_survey_run1.R
@@ -4,40 +4,74 @@ library(tidyverse)
 library(lubridate)
 library(here)
 library(readxl)
+library(janitor)
 
 
 data <- read.csv(here('round2_rows.csv'))
-# dropouts <- read_excel(here('dropouts.xlsx'))
-#
-# unique(dropouts$`Drop Out IDs`)
-#
+dropouts <- read_excel(here('new_dropouts.xlsx')) %>%
+  clean_names()
+
+
+data_joined <- data %>%
+  inner_join(dropouts, by = c("psid" = "drop_out_i_ds"))
+
+
+
 # data_joined <- data %>%
-#   inner_join(dropouts, by = c("psid" = "Drop Out IDs"))
-#
-#
-#
-# data_joined <- data_joined %>%
 #   mutate(get_start_datetime = ymd_hms(time_start, tz = "UTC")) %>%
-#   filter(get_start_datetime > as.POSIXct("2025-10-02 01:22:57", tz = "UTC"))
+#   filter(get_start_datetime > as.POSIXct("2025-10-21 00:00:00", tz = "UTC"))
 
 
 
-user_status <- data_joined %>%
-  group_by(current_page) %>%
+# user_status <- data_joined %>%
+#   group_by(current_page) %>%
+#   summarise(count = n()) %>%
+#   arrange(desc(count))
+
+data_joined$current_page[data_joined$current_page == ""] <- "first_page"
+
+dropout_results <- data_joined %>%
+  mutate(get_start_datetime = ymd_hms(time_start, tz = "UTC"),
+         get_start_date = as_date(get_start_datetime)) %>%
+  filter(get_start_datetime > as.POSIXct("2025-10-21 00:00:00", tz = "UTC")) %>%
+  #filter(get_start_datetime < as.POSIXct("2025-10-27 00:00:00", tz = "UTC")) %>%
+  mutate(
+    # survey_status = case_when(current_page == 'screenout' ~ 'screenout',
+    #                                current_page == 'end' ~ 'successfully completed',
+    #                                .default = 'left the survey'),
+    page_position = case_when(current_page == 'first_page' ~ 'first_page',
+                                   current_page == 'consent' ~ 'second_page',
+                                   current_page == 'screenout' ~ 'screenout',
+                                   .default = 'other_pages')) %>%
+  #group_by(survey_status, get_start_date ) %>%
+  group_by(page_position, get_start_date ) %>%
   summarise(count = n()) %>%
-  arrange(desc(count))
+  pivot_wider(names_from = page_position, values_from = count)
 
 
-data %>%
-  mutate(get_start_datetime = ymd_hms(time_start, tz = "UTC")) %>%
-  filter(get_start_datetime > as.POSIXct("2025-10-10 20:00:00", tz = "UTC")) %>%
-  group_by(current_page) %>%
+
+all_results <- data  %>%
+  mutate(get_start_datetime = ymd_hms(time_start, tz = "UTC"),
+         get_start_date = as_date(get_start_datetime)) %>%
+  filter(get_start_datetime > as.POSIXct("2025-10-21 00:00:00", tz = "UTC")) %>%
+  #filter(get_start_datetime < as.POSIXct("2025-10-27 00:00:00", tz = "UTC")) %>%
+  mutate(
+    survey_status = case_when(current_page == 'screenout' ~ 'screenout',
+                                   current_page == 'end' ~ 'successfully completed',
+                                   .default = 'left the survey')
+    # page_position = case_when(current_page == 'first_page' ~ 'first_page',
+    #                           current_page == 'consent' ~ 'second_page',
+    #                           current_page == 'screenout' ~ 'screenout',
+    #                           .default = 'other_pages')
+    ) %>%
+  group_by(survey_status, get_start_date ) %>%
+  #group_by(page_position, get_start_date ) %>%
   summarise(count = n()) %>%
-  arrange(desc(count))
+  pivot_wider(names_from = survey_status, values_from = count)
 
 
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
+
+
+
